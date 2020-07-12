dc7459536adb5cd59e38afd2b57048469297f5b7
guillermo <guille.bozza@gmail.com>
2020-07-12 19:06:05 -0300

    Transaccion en modelo persona y abm

--- Fisica/ModeloConexion.vb
+++ Fisica/ModeloConexion.vb
@@ -1,4 +1,28 @@
 ﻿Imports System.Data.Odbc
-Public Class ModeloConexion
+Public MustInherit Class ModeloConexion
 
+    Public DbDriver As String = "MySQL ODBC 8.0 UNICODE Driver"
+    Public DbUser As String = "root"
+    Public DbPassword As String = "Tooroot123654"
+    Public DbPort As String = "3306"
+    Public DbName As String = "practicopiola"
+    Public DbHost As String = "localhost"
+
+    Public Conexion As New OdbcConnection("DRIVER=" + Me.DbDriver +
+                                          ";UID=" + Me.DbUser +
+                                          ";PWD=" + Me.DbPassword +
+                                          ";PORT=" + Me.DbPort +
+                                          ";DATABASE=" + Me.DbName +
+                                          ";SERVER=" + Me.DbHost)
+
+    Public Comando As New OdbcCommand
+    Public Reader As OdbcDataReader
+
+    Public Sub New()
+        Me.Conexion.Open()
+        Me.Comando.Connection = Me.Conexion
+
+    End Sub
 End Class
+
+
---
+++ Fisica/Modelos/Persona.vb
@@ -0,0 +1,49 @@
+﻿Public Class Persona
+    Inherits ModeloConexion
+
+    Public Id As String
+    Public Nombre As String
+    Public Apellido As String
+    Public Mail As String
+    Public FechaHoraCreacion As String
+    Public Activo As Boolean
+    Public Telefono As String
+
+
+
+    Public Function Contar()
+        Me.Comando.CommandText = "SELECT COUNT(*) FROM persona"
+        Return Me.Comando.ExecuteScalar().ToString()
+
+    End Function
+
+    Public Sub Guardar()
+        Comando.CommandText = "BEGIN WORK;"
+
+
+        Try
+            Comando.CommandText = "INSERT INTO persona VALUES(" + Me.Id + " ','" + Me.Nombre + "','" + Me.Apellido + "','" + Me.Mail + "','" + Me.FechaHoraCreacion + "','" + Me.Activo + ")"
+            Comando.CommandText = "INSERT INTO personaTel VALUES(" + Me.Id + "','" + Me.Telefono + ")"
+            Comando.ExecuteNonQuery()
+            Comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Comando.CommandText = "ROLLBACK;"
+            Comando.ExecuteNonQuery()
+            MsgBox("Intente de nuevo")
+
+        End Try
+        Comando.CommandText = "COMMIT WORK;"
+        Comando.ExecuteNonQuery()
+
+        MsgBox("Persona agregada")
+
+    End Sub
+
+
+    Public Sub Borrar()
+        Comando.CommandText = "DELETE FROM personas WHERE ci = " + Me.Id
+    End Sub
+
+
+
+End Class
