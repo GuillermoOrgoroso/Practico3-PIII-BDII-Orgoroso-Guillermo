7a7cd222bcb850fa81cee7b6648f6f6856128e14
guillermo <guille.bozza@gmail.com>
2020-07-12 19:51:26 -0300

    Abm Servicio (inicio de abm, solo molde.)

---
+++ Fisica/Modelos/Servicio.vb
@@ -0,0 +1,33 @@
+﻿Public Class Servicio
+    Inherits ModeloConexion
+
+    Public Id As String
+    Public Nombre As String
+    Public CostoMnesual As String
+    Public Tipo As String
+    Public FechaHoraCreacion As String
+    Public Activo As Boolean
+
+
+
+    Public Function listar()
+
+    End Function
+
+
+    Public Sub Guardar()
+
+    End Sub
+
+    Public Sub Borrar()
+
+    End Sub
+
+    Public Function Modificar()
+
+    End Function
+
+    Public Function Contar()
+
+    End Function
+End Class
