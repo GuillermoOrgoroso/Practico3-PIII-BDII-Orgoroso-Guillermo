724d0cfd71ca9af48cbe11f9ff956040c9ddf0a2
guillermo <guille.bozza@gmail.com>
2020-07-12 20:27:30 -0300

    ABM de modelo servicios completo, con control de errores try and catch

--- Fisica/Modelos/Servicio.vb
+++ Fisica/Modelos/Servicio.vb
@@ -3,7 +3,7 @@
 
     Public Id As String
     Public Nombre As String
-    Public CostoMnesual As String
+    Public CostoMensual As String
     Public Tipo As String
     Public FechaHoraCreacion As String
     Public Activo As Boolean
@@ -12,22 +12,63 @@
 
     Public Function listar()
 
+        Comando.CommandText = "SELECT * FROM Servicio"
+        Comando.ExecuteReader()
+        Return Reader
+
     End Function
 
 
     Public Sub Guardar()
+        Try
+
+            Comando.CommandText = "INSERT INTO Servicio VALUES(" + Me.Id + "','" + Me.Nombre + "','" + Me.CostoMensual + " ',' " + Me.Tipo + "','" + Me.FechaHoraCreacion + "','" + Me.Activo + " )"
+            Comando.ExecuteNonQuery()
+            MsgBox("Servicio ingresado al sistema con exito.")
+
+        Catch ex As Exception
+            MsgBox("Error al intentar dar de alta al servicio, intente de nuevo.")
+
+        End Try
+
 
     End Sub
 
     Public Sub Borrar()
+        Try
+            Comando.CommandText = "DELETE FROM Servicio WHERE ID = " + Me.Id
+            Comando.ExecuteNonQuery()
+            MsgBox("Servicio eliminado con exito.")
+
+        Catch ex As Exception
+            MsgBox("Error al intentar eliminar el servicio del sistema.")
+        End Try
+
+
 
     End Sub
 
     Public Function Modificar()
+        Try
+            Comando.CommandText = "UPDATE Servicio SET Nombre = '" + Me.Nombre + " ',CostoMensual = '" + Me.CostoMensual + "', Tipo = '" + Me.Tipo + "', FechaHoraCreacion = '" + Me.FechaHoraCreacion + "', Activo ='" + Me.Activo + "' WHERE ID ='" + Me.Id
+            Comando.ExecuteReader()
+
+            Return Reader
+            MsgBox("Datos del servicio modificados con exito.")
+
+        Catch ex As Exception
+            MsgBox("Error al intentar modificar los datos del servicio")
+
+        End Try
+
+        Return -1
 
     End Function
 
     Public Function Contar()
+
+        Me.Comando.CommandText = "SELECT COUNT(*) FROM Servicio"
+        Return Me.Comando.ExecuteScalar().ToString()
 
     End Function
 End Class
