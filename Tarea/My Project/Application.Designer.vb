9a8faee7c18724388bea014b6df1e4458398b9bb
guillermo <guille.bozza@gmail.com>
2020-07-13 18:22:01 -0300

    Agregada interface MAIN y controladorContrata

--- Tarea/My Project/Application.Designer.vb
+++ Tarea/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Tarea.Form1
+            Me.MainForm = Global.Tarea.Main
         End Sub
     End Class
 End Namespace
